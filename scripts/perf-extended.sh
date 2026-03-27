41930ed9df55969cc0cc9514bd08d17195d29a0e
dave <dave@dev.srvc.au>
2026-03-27 21:34:21 +1100

    Remove Makefile, scripts, examples, and VERSION file

--- scripts/perf-extended.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-
-go test -v -run 'TestPerfTable(Operations|100K|Concurrent)$' ./...
--- scripts/verify-all.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-
-"$(dirname "$0")/test-critical.sh"
-"$(dirname "$0")/bench-core.sh"
